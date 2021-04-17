f87fc314bd143e1a580affe22e79f7fd7cc08b9e
Bráulio Figueiredo <braulio@braulioti.com.br>
2021-04-17 11:17:53 -0300

    Added Vibrant.js library and extract colors from images

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as cron from 'node-cron';
 import * as fs from 'fs';
+// @ts-ignore
+import * as Vibrant from 'node-vibrant';
 import {Database} from './common/database';
 import {paramsDB, photoDir, timer} from './common/environment';
 import {PhotoModel, PhotoService} from './service/photo.service';
@@ -13,25 +15,46 @@ database.connect();
 
 const photoService = new PhotoService(database);
 
-function processCron() {
+function getPallet(folder): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+        Vibrant.from(folder).getPalette().then((palette) => {
+            resolve(palette);
+        }).catch(e => {
+            reject(e);
+        })
+    });
+}
 
+async function processCron() {
     const files = fs.readdirSync(photoDir);
-    files.forEach(file => {
-        console.log(file);
+
+    let promises: any;
+
+    promises = files.map(async (file) => {
         if (file.indexOf('.jpeg')) {
             const image = fs.readFileSync(`${photoDir}/${file}`);
 
+            const pallet = await getPallet(`${photoDir}/${file}`);
+
             const photo: PhotoModel = new PhotoModel();
             photo.photo = new Buffer(image).toString('base64');
+            photo.vibrant = pallet.Vibrant.hex;
+            photo.darkVibrant = pallet.DarkVibrant.hex;
+            photo.lightVibrant = pallet.LightVibrant.hex;
+            photo.muted = pallet.Muted.hex;
+            photo.darkMuted = pallet.DarkMuted.hex;
 
+            console.log(photo);
             fs.unlinkSync(`${photoDir}/${file}`);
         }
     });
+    await Promise.all(promises);
 }
 
 cron.schedule(timer, () => {
     processCron();
 });
+
 
 app.use('/assets', express.static(`${__dirname}/../views/assets`));
 app.use(bodyParser.urlencoded({ extended: true }));
